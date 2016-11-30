e3d66957807277fd6f47dce02d82480f522f3607
ctacuri <cesar_hbk@hotmail.com>
2016-11-29 23:31:38 -0500

    Actualizacion de Activity Menu y implementacion de Fabric - Crashlytics

--- app/src/main/java/urp/arch/simuladorcredito/MainActivity.java
+++ app/src/main/java/urp/arch/simuladorcredito/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
     }
 
